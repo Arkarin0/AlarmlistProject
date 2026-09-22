2629bb305375f32bdb2761f41f9a0a26ac08a5aa
Arkarin0 <snaut@hotmail.de>
2026-09-22 22:47:14 +0200

    added enpty project for the Visual studio integration.

---
+++ src/VisualStudio/Alarmlist.VisualStudio/EditFileControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿// Created/modified by Arkarin0 under one ore more license(s).
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Alarmlist.VisualStudio
+{
+    /// <summary>
+    /// Interaktionslogik für EditFileControl.xaml
+    /// </summary>
+    public partial class EditFileControl : UserControl
+    {
+        public EditFileControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/VisualStudio/Alarmlist.VisualStudio/UI/EditFileControlViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarmlist.UI
+{
+    internal class EditFileControlViewModel
+    {
+    }
+}
